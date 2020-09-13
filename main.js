1b94c57f17e3e2d62be708381b08d155b0fea9b0
Sourav Gain <souravgain605@gmail.com>
2020-09-13 15:04:38 +0530

    added light mode

--- main.js
+++ main.js
@@ -1,3 +1,70 @@
+var dark=true;
+
+function switchMode(){
+    if(dark){
+        document.getElementById("switchMode").innerHTML='<i class="fas fa-lightbulb"></i>';
+        document.documentElement.style.setProperty("--primary-background-color" , "#f8f8f8");
+        document.documentElement.style.setProperty("--secondary-background-color" , "#f3f3f3");
+
+        document.documentElement.style.setProperty("--button-bg-color" , "#eaeaea");
+        document.documentElement.style.setProperty("--button-border-color" , "#e0e0e0");
+
+        document.documentElement.style.setProperty("--basic-text-color" , "#303030");
+        document.documentElement.style.setProperty("--disabled-ico-color" , "#3f546f");
+
+
+        document.documentElement.style.setProperty("--light-blue-color" , "#2196F3");
+        document.documentElement.style.setProperty("--light-green-color" , "#4CAF50");
+        document.documentElement.style.setProperty("--light-orange-color" , "#FF9800");
+        document.documentElement.style.setProperty("--light-red-color" , "#F44336");
+        document.documentElement.style.setProperty("--light-purple-color" , "#4e41ff");
+        document.documentElement.style.setProperty("--light-pink-color" , "#ff34c2");
+
+        document.documentElement.style.setProperty("--light-semi-blue-color" , "#2196F3");
+        document.documentElement.style.setProperty("--light-semi-green-color" , "#4CAF50");
+        document.documentElement.style.setProperty("--light-semi-orange-color" , "#FF9800");
+        document.documentElement.style.setProperty("--light-semi-red-color" , "#ff3e31");
+        document.documentElement.style.setProperty("--light-semi-purple-color" , "#4e41ff");
+        document.documentElement.style.setProperty("--light-semi-pink-color" , "#ff34c2");
+
+
+        document.documentElement.style.setProperty("--nameFilter" , "invert(41%) sepia(62%) saturate(5332%) hue-rotate(297deg) brightness(103%) contrast(101%)");
+        document.documentElement.style.setProperty("--emailFilter" , "invert(21%) sepia(79%) saturate(3700%) hue-rotate(242deg) brightness(100%) contrast(103%)");
+        document.documentElement.style.setProperty("--msgFilter" , "invert(55%) sepia(62%) saturate(416%) hue-rotate(73deg) brightness(94%) contrast(91%)");
+        dark=false;
+    }else{
+        document.getElementById("switchMode").innerHTML='<i class="far fa-lightbulb">';
+        document.documentElement.style.setProperty("--primary-background-color" , "#232931");
+        document.documentElement.style.setProperty("--secondary-background-color" , "#292F39");
+
+        document.documentElement.style.setProperty("--button-bg-color" , "#323B49");
+        document.documentElement.style.setProperty("--button-border-color" , "#2E3746");
+
+        document.documentElement.style.setProperty("--basic-text-color" , "#E3E3E3");
+        document.documentElement.style.setProperty("--disabled-ico-color" , "#637183");
+
+        document.documentElement.style.setProperty("--light-blue-color" , "#BDF3FF");
+        document.documentElement.style.setProperty("--light-green-color" , "#bdffdb");
+        document.documentElement.style.setProperty("--light-orange-color" , "#ffeabd");
+        document.documentElement.style.setProperty("--light-red-color" , "#ffc1bd");
+        document.documentElement.style.setProperty("--light-purple-color" , "#c1bdff");
+        document.documentElement.style.setProperty("--light-pink-color" , "#ffbdeb");
+
+        document.documentElement.style.setProperty("--light-semi-blue-color" , "#bdf3ffa4");
+        document.documentElement.style.setProperty("--light-semi-green-color" , "#bdffdba4");
+        document.documentElement.style.setProperty("--light-semi-orange-color" , "#ffeabda4");
+        document.documentElement.style.setProperty("--light-semi-red-color" , "#ffc1bda4");
+        document.documentElement.style.setProperty("--light-semi-purple-color" , "#c1bdffa4");
+        document.documentElement.style.setProperty("--light-semi-pink-color" , "#ffbdeba4");
+
+        document.documentElement.style.setProperty("--nameFilter" , "invert(71%) sepia(31%) saturate(579%) hue-rotate(292deg) brightness(114%) contrast(106%)");
+        document.documentElement.style.setProperty("--emailFilter" , "invert(71%) sepia(6%) saturate(1807%) hue-rotate(204deg) brightness(104%) contrast(105%)");
+        document.documentElement.style.setProperty("--msgFilter" , "invert(89%) sepia(24%) saturate(401%) hue-rotate(83deg) brightness(103%) contrast(104%)");
+        dark=true;
+    }
+}
+
+
 function sleep(ms){
     return new Promise(resolve => setTimeout(resolve,ms));
 }

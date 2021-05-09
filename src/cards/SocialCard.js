8da79c0b13b1eefa55ebb511008f011aed9032cf
Sourav Gain <souravgain605@gmail.com>
2021-05-09 14:04:59 +0530

    fix bug for mobile side

--- src/cards/SocialCard.js
+++ src/cards/SocialCard.js
@@ -1,14 +1,20 @@
 import React from "react";
 
 export default function SocialCard({ logo, heading, subtext, url }) {
+  const link = () => {
+    const a = document.createElement("a");
+    a.target = "blank";
+    a.href = url;
+    a.click();
+  };
   return (
     <div className="socialCard" initial="hidden" animate="show">
       <div className="mContainer">
         <h1>{heading}</h1>
         <p>{subtext}</p>
-        <a href={url} target="blank" className="cardbtnContainer">
-          <i className={`${logo} accM`}></i>
-        </a>
+        <div href={url} target="blank" className="cardbtnContainer">
+          <i onClick={() => link()} className={`${logo} accM`}></i>
+        </div>
       </div>
       <div className="lContainer">
         <i className={logo}></i>
